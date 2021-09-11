553c07e074f04d693461551d2a4e6f29c526a855
Sergey Skidanov <vonadiks@mail.ru>
2021-09-11 23:28:18 +0400

    initial commit

---
+++ src/main/java/com/example/spring2cloud/Spring2cloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring2cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2cloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2cloudApplication.class, args);
+	}
+
+}
